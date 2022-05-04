ea546c9ca6d3a5b91236112612c18baeff7398a5
Ori Guy <origuy2021@gmail.com>
2022-05-04 17:39:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const fullName = document.querySelector("#name");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const lang = document.querySelector("#lang");
+const btn = document.querySelector("#btn");
+const card = document.querySelector(".card");
+
+btn.addEventListener("click", () => {
+    fetch("http://localhost:3002/profile").then(response =>response.json()).then(data => {
+        showProfile(data)
+     })
+})
+
+
+let showProfile = (profile) => {
+    setTimeout(() => {
+        fullName.innerHTML += ` ${profile.fullName}`;
+        username.innerHTML += ` ${profile.userName}`;
+        email.innerHTML += ` ${profile.mail}`;
+        profile.languages.forEach(languages => {
+            lang.innerHTML += ` ${languages} <span style="font-weight: 900;"> | </span>
+            `
+        });
+        
+    }, 300);
+}
